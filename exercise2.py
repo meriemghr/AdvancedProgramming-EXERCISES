4e23a01f56d1fbb088c58ee0d08a390904244985
meriemghr <115370911+meriemghr@users.noreply.github.com>
2025-01-17 14:26:55 +0100

    Create exercise2.py

---
+++ exercise2.py
@@ -0,0 +1,11 @@
+temp = int(input("Please type in the temperature: "))
+
+if temp < 20:
+    print("It's cool!")
+if temp < 10:
+    print("It's cold!")
+if temp < 0:
+    print("It's freezing!")
+
+# Final message
+print("Stay safe!")
