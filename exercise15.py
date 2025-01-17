bfc047d013e3f28384adc37a6bf05ed2e759222a
meriemghr <115370911+meriemghr@users.noreply.github.com>
2025-01-17 15:11:21 +0100

    Create exercise15.py

---
+++ exercise15.py
@@ -0,0 +1,8 @@
+user_input = input("Please type in a string: ")
+vowels = ['a', 'e', 'o']
+
+for vowel in vowels:
+    if vowel in user_input:
+        print(f"{vowel} found")
+    else:
+        print(f"{vowel} not found")
