f4ca8c7e5574d38a490ddec191eede1e3b49084e
meriemghr <115370911+meriemghr@users.noreply.github.com>
2025-01-17 14:57:58 +0100

    Create exercise10.py

---
+++ exercise10.py
@@ -0,0 +1,12 @@
+word = input("Please type a word: ")
+is_palindrome = True
+
+for i in range(len(word) // 2):
+    if word[i] != word[-(i + 1)]:
+        is_palindrome = False
+        break
+
+if is_palindrome:
+    print("Yes, it's a palindrome.")
+else:
+    print("No, it's not a palindrome.")
